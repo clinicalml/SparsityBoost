4f60930e092c87bf2a3dfe0babda5a6d371190d0
eliot <eliotpbrenner@gmail.com>
2013-08-04 13:37:01 -0400

    run_experiment

--- perl/run_experiment.pl
+++ perl/run_experiment.pl
@@ -13,65 +13,61 @@
 #$outputdir = 'tmp_BIC';
 
 $datadir = '../data/synthetic_examples/experiments/';
-$outputdir = 'results/experiments/';
+$outputdir = '../results/experiments/';
 $method = 'sparsity_eps005';
 
 for($exp=0; $exp<=9; $exp++) {
 
-print "\n\nEXPERIMENT #$exp\n\n";
+	print "\n\nEXPERIMENT #$exp\n\n";
 
-$datadir_full = $datadir . $exp . "/";
-$outputdir_partial = $outputdir . $exp;
-$outputdir_full = $outputdir . $exp . "/" . $method;
-if(!(-d $outputdir_partial)) {
-    mkdir($outputdir_partial);
-}
-if(!(-d $outputdir_full)) {
-    mkdir($outputdir_full);
-}
+	$datadir_full = $datadir . $exp . "/";
+	$outputdir_partial = $outputdir . $exp;
+	$outputdir_full = $outputdir . $exp . "/" . $method;
+	if(!(-d $outputdir_partial)) {
+		mkdir($outputdir_partial);
+	}
+	if(!(-d $outputdir_full)) {
+		mkdir($outputdir_full);
+	}
 
-$N_start = 4400;
-$N_end = 8000;
-$N_increment = 400;
+	$N_start = 4400;
+	$N_end = 8000;
+	$N_increment = 400;
 
-for($N=$N_start; $N<=$N_end; $N+=$N_increment) {
+	for($N=$N_start; $N<=$N_end; $N+=$N_increment) {
 
-    print "\n$N\n";
+		print "\n$N\n";
+		
+		$time_file = $outputdir_full . "/bscore_time$N.dat";
+		$model_file = $outputdir_full . "/model$N.mod";
+		$mec_file = $outputdir_full . "/output$N.txt";
+		$mec_file2 = $outputdir_full . "/bn$N.mec";
 
-#    $data_file = $outputdir_full . "/data$N.dat";
-    $time_file = $outputdir_full . "/bscore_time$N.dat";
-    $model_file = $outputdir_full . "/model$N.mod";
-    $mec_file = $outputdir_full . "/output$N.txt";
-    $mec_file2 = $outputdir_full . "/bn$N.mec";
+		$data_file = $datadir_full . "alarm" . $N . ".dat";
 
-    # Cut the first N lines from $data file
-#    `head -n $N $data > $data_file`;
-    $data_file = $datadir_full . "alarm" . $N . ".dat";
+		# Create model file from this
+		$start_time = time;
+		#    `./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file`;
+		#    #    `./c2/bscore -alpha 10 -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file`;
+		`./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .005`;
+		#    `./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .005`;
+		print "./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .02\n";
+		print " ";
 
-    # Create model file from this
-    $start_time = time;
-#    `./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file`;
-#    `./c2/bscore -alpha 10 -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file`;
-    `./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .005`;
-#    `./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .005`;
-    print "./c2/bscore -bic -nodes 37 -maxpa 4 -data $data_file -mod_out $model_file -edge_scores .02\n";
-	print " ";
+		$end_time = time;
+		$total_time = $end_time - $start_time;
+		open(FILE, ">$time_file");
+		print FILE $total_time;
+		close(FILE);
 
-    $end_time = time;
-    $total_time = $end_time - $start_time;
-    open(FILE, ">$time_file");
-    print FILE $total_time;
-    close(FILE);
+		# Create settings file
+		`cp gobnilp.set $outputdir_full/gobnilp$N.set`;
+		`sed -i '' 's|%DIR|$outputdir_full|' $outputdir_full/gobnilp$N.set`;  #TODO: determine meaning of '' here
+		`sed -i '' 's/%N/$N/' $outputdir_full/gobnilp$N.set`;
 
-    # Create settings file
-    `cp gobnilp.set $outputdir_full/gobnilp$N.set`;
-    `sed -i '' 's|%DIR|$outputdir_full|' $outputdir_full/gobnilp$N.set`;
-    `sed -i '' 's/%N/$N/' $outputdir_full/gobnilp$N.set`;
+		# Run (note, need "gobnilp.set") in same directory
+		print "gobnilp1.3/bin/gobnilp -g$outputdir_full/gobnilp$N.set $model_file > $mec_file";
+		`gobnilp1.3/bin/gobnilp -g$outputdir_full/gobnilp$N.set $model_file > $mec_file`;
 
-    # Run (note, need "gobnilp.set") in same directory
-    print "gobnilp1.3/bin/gobnilp -g$outputdir_full/gobnilp$N.set $model_file > $mec_file";
-    `gobnilp1.3/bin/gobnilp -g$outputdir_full/gobnilp$N.set $model_file > $mec_file`;
-
-#    print `grep -A100000  'START MEC' $mec_file > $mec_file2 && diff $mec_file2 ../data/synthetic_examples/alarm_network.MEC`;
-}
+	}
 }
